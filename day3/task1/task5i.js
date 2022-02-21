d973de6ee583e491c734d8e0fbe6c6218f8dddf8
tkasvala9 <twinkalkasvala@gmail.com>
2022-02-21 16:57:16 +0530

    day3

--- day3/task1/task5i.js
+++ day3/task1/task5i.js
@@ -5,12 +5,10 @@ function age(){
     var bdate=document.getElementById("bdate").value;
     var bdatee=new Date(bdate);
 
-    console.log(bdate);
-
     var year=date.getFullYear();
-    console.log(year);
+    
     var birthyear=bdatee.getFullYear();
-    console.log(birthyear);
+   
     var personage=year-birthyear;
     console.log(personage);
     
