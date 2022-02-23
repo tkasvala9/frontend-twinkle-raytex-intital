c6e1bf2019237c01a55892a4aa93eabfa4f7b761
tkasvala9 <twinkalkasvala@gmail.com>
2022-02-23 23:35:49 +0530

    update-task6

--- day3/task6/cart.js
+++ day3/task6/cart.js
@@ -1,7 +1,13 @@
-var count=0;
+var count = 0;
 function addcart(){
    
     count++;
-    console.log(count);
     
+    console.log(count);
+    localStorage.setItem("count",count)
+    
+}
+
+function cartload(){
+    document.getElementById("counter1").innerHTML = localStorage.getItem("count");
 }
